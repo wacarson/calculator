c9edfdf8b8d6cd90d47280b7884f80435fdc28f8
Bill Carson <35587777+wacarson@users.noreply.github.com>
2024-07-07 20:48:36 -0400

    Parentheses seem to be fully working. Last upload was broken. Changed display bar styling a bit: larger font, border, sans-serif.

--- calc.js
+++ calc.js
@@ -1,10 +1,12 @@
-/*TO DO
-Add parentheses support
+/*GOALS
+Add exponents
+Add keyboard support
+Use correct PEMDAS sequence
 */
 
 //globals
 let operator = ''
-let nonNums = ['+', '-', '*', '/', '(', ')']
+let opsList = ['+', '-', '*', '/']
 let sequence = []
 let state = false
 let lock = false
@@ -56,7 +58,7 @@ function buttonPress(e) {
 		}
 	//checks if the display is the result of prior math
 		state = false
-		if (nonNums.includes(button)) {
+		if (opsList.includes(button)) {
 			currentNum = screen.textContent
 			sequence.push(currentNum)
 			//uses result as initial number if operator pressed
@@ -81,7 +83,13 @@ function buttonPress(e) {
 		return
 	} else if (button == '=') {
 		sequence.pop() //remove the equals sign and deal with data
-		parse()
+		if (checkErrors()) {
+			return
+		}
+		number = parse(sequence)
+		clear()
+		display(number)
+		state = true
 		return
 	}
 	display(button)			//show button press
@@ -142,27 +150,17 @@ function backspace() {
 function syntaxCheck() {
 	x = sequence.length
 	flag = false
-	if (nonNums.includes(sequence[x-1])){
+	if (opsList.includes(sequence[x-1])){
 		flag = true
-		if (sequence[x-1] == ")") {
-			flag = false
-		}
-	} else if (nonNums.includes(sequence[0])) {
-		flag = true
-		if (sequence[0] == "(") {
-			flag = false
-		}
 	}
-
+	if (opsList.includes(sequence[0])) {
+		flag = true
+	}
 	for (let i=1; i<x-1; i++) {
-		firstcase = nonNums.includes(sequence[i-1])
-		secondcase = nonNums.includes(sequence[i])
-		ptest = (sequence[i-1] == '(' || sequence[i-1] == ')')
+		firstcase = opsList.includes(sequence[i-1])
+		secondcase = opsList.includes(sequence[i])
 		if (firstcase && secondcase) {
 			flag = true
-			if (ptest && sequence[i-1] == sequence[i]) {
-				flag = false
-			}
 		}
 	}
 //all these flag = false statements would be breaks if allowed
@@ -195,7 +193,7 @@ function divByZero() {
 
 	for (let i=0; i<divByZeroCheck.length; i++) {
 		check = divByZeroCheck[i] + 1
-		if (!nonNums.includes(sequence[check]) || sequence[check] != '.') {
+		if (!opsList.includes(sequence[check]) || sequence[check] != '.') {
 			clear()
 			display("Do not divide by zero. Hit Clear")
 			lock = true
@@ -207,25 +205,29 @@ function divByZero() {
 
 function checkParentheses() {
 	leftpLoc = []
-	rightpLoc = []
+	pcount = 0
 	failflag = false
-	for(let i=0; i < sequence.length-1; i++) {
+	for(let i=0; i <= sequence.length-1; i++) {
 		if (sequence[i] == '(') {
+			pcount++
 			leftpLoc.push(i)
-			if (nonNums.includes(sequence[i+1])) {
+			if (opsList.includes(sequence[i+1])) {
 				failflag = true
 				break
 			}
 		} else if (sequence[i] == ')') {
-			rightpLoc.push(i)
-			if (nonNums.includes(sequence[i-1])) {
+			pcount++
+			left = leftpLoc.pop()
+			right = i
+			if (opsList.includes(sequence[i-1])) {
 				failflag = true
 				break
 			}
+			handleParentheses(left+1, right)
 		}
 	}
 
-	if (leftpLoc.length != rightpLoc.length) {
+	if (pcount % 2 != 0) {
 		failflag = true
 	}
 
@@ -235,28 +237,36 @@ function checkParentheses() {
 		lock = true
 		return true
 	}
+}
 
-	handleParentheses(leftpLoc, rightpLoc)
+
+
+function handleParentheses(left, right) {
+	subarray = sequence.slice(left,right)
+	result = parse(subarray)
+	sequence[left-1] = result
+	for (let i=left; i<=right; i++) {
+		sequence[i] = ''
+	}
+}
+
+function checkErrors() {
+	if (syntaxCheck()) {
+		return true
+	} else if (divByZero()) {
+		return true
+	}
+
+	if (checkParentheses()) {
+		return true
+	}
+
 	return false
 }
 
-
-function handleParentheses(lefts, rights) {
-	return
-}
-
 //work out the math function from the text
-function parse() {
-	if (syntaxCheck()) {
-		return
-	} else if (divByZero()) {
-		return
-	}
-
-	if (checkParentheses) {
-		return
-	}
-	equation = sequence.toString() //convert array to one string
+function parse(maths) {
+	equation = maths.toString() //convert array to one string
 	equation = equation.replaceAll(',', '') //remove commas from conversion
 	num1 = ''
 	num2 = ''
@@ -265,16 +275,16 @@ function parse() {
 	for(let i=0; i<equation.length; i++) {
 		bit = equation[i]
 
-		if (!nonNums.includes(bit) && !flag) {
+		if (!opsList.includes(bit) && !flag) {
 			if (bit == '?') { bit = '-' }
 			num1 += bit
-		} else if (!nonNums.includes(bit) && flag) {
+		} else if (!opsList.includes(bit) && flag) {
 			if (bit == '?') { bit = '-' }
 			num2 += bit
-		} else if (nonNums.includes(bit) && !flag) {
+		} else if (opsList.includes(bit) && !flag) {
 			operator = bit
 			flag = true
-		} else if (nonNums.includes(bit) && flag) {
+		} else if (opsList.includes(bit) && flag) {
 			//if a second operand is used, complete first equationa nd continue
 			num1 = '' + operate(parseFloat(num1), parseFloat(num2), operator)
 			num2 = ''
@@ -282,7 +292,6 @@ function parse() {
 		}
 	}
 	result = operate(parseFloat(num1), parseFloat(num2), operator)
-	clear()
-	display(result)
-	state = true
+	result = Math.round(result*10000)/10000
+	return result
 }
